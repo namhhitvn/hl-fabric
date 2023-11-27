f5a8d4c595e186e4ae32e0f77f9136a6154a8791
Nam Hoang <namhh.itvn@gmail.com>
2023-11-27 10:24:32 +0700

    feat: clone kvdb from leveldb

--- common/ledger/util/kvdbhelper/kvdb_provider.go
+++ common/ledger/util/kvdbhelper/kvdb_provider.go
@@ -1,10 +1,4 @@
-/*
-Copyright IBM Corp. All Rights Reserved.
-
-SPDX-License-Identifier: Apache-2.0
-*/
-
-package leveldbhelper
+package kvdbhelper
 
 import (
 	"bytes"
@@ -36,13 +30,6 @@ var (
 // closeFunc closes the db handle
 type closeFunc func()
 
-// Conf configuration for `Provider`
-//
-// `ExpectedFormat` is the expected value of the format key in the internal database.
-// At the time of opening the db, A check is performed that
-// either the db is empty (i.e., opening for the first time) or the value
-// of the formatVersionKey is equal to `ExpectedFormat`. Otherwise, an error is returned.
-// A nil value for ExpectedFormat indicates that the format is never set and hence there is no such record.
 type Conf struct {
 	DBPath         string
 	ExpectedFormat string
